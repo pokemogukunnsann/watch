23930b0a82cf1de9b8508444debb0a167b478412
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-04-20 13:47:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def watch_video():
         
         # 動画再生部分のHTMLに置き換える
         video_html = f'''
-        <video controls="" autoplay="" poster="https://img.youtube.com/vi/{videoid}/0.jpg" width="640" height="360">
+        <!--<video controls="" autoplay="" poster="https://img.youtube.com/vi/{videoid}/0.jpg" width="640" height="360">
             <source src="https://inv-eu2-c.nadeko.net/api/manifest/dash/id/QJo9X5riyWE?local=true&unique_res=1&check=" type="application/dash+xml" label="dash">
             <source src="https://inv-ca1-c.nadeko.net/latest_version?id={videoid}&itag=18&check=" type="video/mp4">
             <source src="https://inv-us2-c.nadeko.net/latest_version?id={videoid}&itag=18&check=" type="video/mp4">
             <source src="https://inv-eu2-c.nadeko.net/latest_version?id={videoid}&itag=18&check=" type="video/mp4">
             <source src="https://inv-eu3-c.nadeko.net/latest_version?id={videoid}&itag=18&check=" type="video/mp4">
             お使いのブラウザは動画タグをサポートしていません。
-        </video>
-        https://www.youtubeeducation.com/embed/{videoid}?embed_config=%7B%22enc%22%3A%22AXH1ezkRiBJkmX6qs8WHisUf72KwPPjLRshwQcdT1lv-pDgxChnF5o4Oi692Y-DeUTO4Y5atxHwwp_P1H7usZTpc7tVgZ_wg0XJ5l8H5YBY1GFcI0kcGVoFoj64vhmZS-dzfjOw7u_9J9LFZejoQ_Ow_R-MuqRkfJQ%3D%3D%22%2C%22hideTitle%22%3Atrue%7D
+        </video>-->
+        <iframe src="https://www.youtubeeducation.com/embed/{videoid}?embed_config=%7B%22enc%22%3A%22AXH1ezkRiBJkmX6qs8WHisUf72KwPPjLRshwQcdT1lv-pDgxChnF5o4Oi692Y-DeUTO4Y5atxHwwp_P1H7usZTpc7tVgZ_wg0XJ5l8H5YBY1GFcI0kcGVoFoj64vhmZS-dzfjOw7u_9J9LFZejoQ_Ow_R-MuqRkfJQ%3D%3D%22%2C%22hideTitle%22%3Atrue%7D" allowfullscreen=""></iframe>
         '''
         
         # 受け取ったHTMLを変更してレスポンスを返す
